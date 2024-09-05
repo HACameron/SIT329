1fcc4a62981445f9e67d35563f197139d30f11cf
HACameron <104876777+HACameron@users.noreply.github.com>
2024-09-05 16:00:36 +1000

    Update TIMER_DEAKIN.h
    started using samd.h library

--- PracticalTask3.1P/BlinkLED/TIMER_DEAKIN.h
+++ PracticalTask3.1P/BlinkLED/TIMER_DEAKIN.h
@@ -3,12 +3,14 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <samd.h>
 
 class TIMER_DEAKIN {
 public:
-    bool config_timer(uint8_t timerNum, uint16_t startValue, uint16_t endValue);
-    uint16_t getTC_count(uint8_t timerNum);
-    void wait(float period_ms);
+    bool config_timer(uint8_t timer_num, uint16_t start_count, uint16_t end_count, uint16_t resolution);
+    uint16_t getTC0_count();
+    // Add other timer count getter methods as needed
+    void wait(uint16_t period); // Period in 0.1ms
 };
 
 #endif // TIMER_DEAKIN_H
