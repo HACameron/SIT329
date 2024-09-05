4d77bd6df47544517c0b6d1826751adf445fab0f
HACameron <104876777+HACameron@users.noreply.github.com>
2024-09-05 16:00:13 +1000

    Update TIMER_DEAKIN.cpp
    started using samd.h library

--- PracticalTask3.1P/BlinkLED/TIMER_DEAKIN.cpp
+++ PracticalTask3.1P/BlinkLED/TIMER_DEAKIN.cpp
@@ -2,51 +2,41 @@
 #include <stdbool.h>
 #include "TIMER_DEAKIN.h"
 
-// Base address of the TC registers
-#define TC_BASE 0x42002000U
+void enable_timer_clock(uint8_t timer_num) {
+  if (timer_num == 0) {
+    PM->APBCMASK.reg |= PM_APBCMASK_TCC0;
+    GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(GCLK_CLKCTRL_ID_TCC0_TCC1_Val) | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_CLKEN;
+    while (GCLK->STATUS.bit.SYNCBUSY);
+  }
+}
 
-// Timer/Counter registers base address
-#define TC_REG_ADDR(num) (TC_BASE + (0x400U * (num)))
+uint32_t calculate_ticks(float period, float resolution) {
+  return (uint32_t)((period / resolution) * (SystemCoreClock / 1000));
+}
 
-// Register offsets
-#define TC_CTRLA_OFFSET 0x00U
-#define TC_CC0_OFFSET   0x44U
-#define TC_COUNT_OFFSET 0x34U
+bool TIMER_DEAKIN::config_timer(uint8_t timer_num, uint16_t start_count, uint16_t end_count, uint16_t resolution) {
+  if (timer_num > 7) return false;
+  
+  enable_timer_clock(timer_num);
 
-// Access registers
-#define TC_CTRLA(num) (*(volatile uint32_t *)(TC_REG_ADDR(num) + TC_CTRLA_OFFSET))
-#define TC_CC0(num) (*(volatile uint32_t *)(TC_REG_ADDR(num) + TC_CC0_OFFSET))
-#define TC_COUNT(num) (*(volatile uint32_t *)(TC_REG_ADDR(num) + TC_COUNT_OFFSET))
-
-// Timer/Counter Control
-#define TC_CTRLA_SWRST (1 << 0) // Software Reset
-#define TC_CTRLA_ENABLE (1 << 1) // Enable
-#define TC_CTRLA_PRESCALER_DIV1 (0x0U << 8) // Prescaler value: DIV1
-
-bool TIMER_DEAKIN::config_timer(uint8_t timerNum, uint16_t startValue, uint16_t endValue) {
-    if (timerNum > 1) return false;
+  if (timer_num == 0) {
+    TCC0->CTRLA.reg = TCC_CTRLA_PRESCALER_DIV1 | TCC_CTRLA_ENABLE;
+    TCC0->PER.reg = calculate_ticks(end_count - start_count, resolution);
     
-    TC_CTRLA(timerNum) = TC_CTRLA_SWRST;
-    while (TC_CTRLA(timerNum) & TC_CTRLA_SWRST);
-
-    TC_CTRLA(timerNum) = TC_CTRLA_PRESCALER_DIV1
-                       | TC_CTRLA_ENABLE;
-    
-    TC_CC0(timerNum) = endValue;
-    TC_COUNT(timerNum) = startValue;
-
-    TC_CTRLA(timerNum) |= TC_CTRLA_ENABLE;
-
+    TCC0->CTRLA.reg |= TCC_CTRLA_ENABLE;
+    while(TCC0->SYNCBUSY.bit.ENABLE);
     return true;
+  }
 }
 
-uint16_t TIMER_DEAKIN::getTC_count(uint8_t timerNum) {
-    if (timerNum > 1) return 0; //
-    return TC_COUNT(timerNum);
+uint16_t TIMER_DEAKIN::getTC0_count() {
+  return TCC0->COUNT.reg;
 }
 
-void TIMER_DEAKIN::wait(float period_ms) {
-    uint16_t startValue = getTC_count(0);
-    uint16_t elapsedTicks = (uint16_t)((period_ms / 0.1) + startValue);
-    while (getTC_count(0) < elapsedTicks);
+void TIMER_DEAKIN::wait(uint16_t period) {
+  config_timer(0, 0, calculate_ticks(period, 0.1), 0.1);
+
+  while(TCC0->COUNT.reg < TCC0->PER.reg);
 }
+
+
