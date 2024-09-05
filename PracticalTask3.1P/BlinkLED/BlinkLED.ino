67fe072045e4a08ed9758d16a3ee0df3a808fc0f
HACameron <104876777+HACameron@users.noreply.github.com>
2024-09-05 15:59:46 +1000

    Update BlinkLED.ino
    Setup 3 LED for next task

--- PracticalTask3.1P/BlinkLED/BlinkLED.ino
+++ PracticalTask3.1P/BlinkLED/BlinkLED.ino
@@ -5,31 +5,47 @@
 GPIO_DEAKIN gpio;
 TIMER_DEAKIN timer;
 
-const char LED_PORT = 'A';
-const char LED_PIN = 21;
+const char LED1_PORT = 'A';
+const char LED1_PIN = 21;
+
+const char LED2_PORT = 'A';
+const char LED2_PIN = 20;
+
+const char LED3_PORT = 'A';
+const char LED3_PIN = 18;
+
+const char DHT_PORT = 'B';
+const char DHT_PIN = 10;
+
 
 void setup() {
     Serial.begin(9600);
 
     Serial.println("Starting setup...");
 
-    gpio.config_GPIO(LED_PORT, LED_PIN, OUTPUT);
+    gpio.config_GPIO(LED1_PORT, LED1_PIN, OUTPUT);
+    gpio.config_GPIO(LED2_PORT, LED2_PIN, OUTPUT);
+    gpio.config_GPIO(LED3_PORT, LED3_PIN, OUTPUT);
     
-    timer.config_timer(0, 0, 500);
+    timer.config_timer(0, 0, 1000, 0.1);
 }
 
 void loop() {
     // Turn the LED on
-    gpio.write_GPIO(LED_PORT, LED_PIN, HIGH);
-    Serial.println("Set PA21 to HIGH");
+    gpio.write_GPIO(LED1_PORT, LED1_PIN, HIGH);
+    gpio.write_GPIO(LED2_PORT, LED2_PIN, HIGH);
+    gpio.write_GPIO(LED3_PORT, LED3_PIN, HIGH);
+    Serial.println("All 3 LED to HIGH");
 
-    // Wait for 500ms (500 * 10 * 0.1ms)
-    timer.wait(500);
+    timer.wait(1000);
+
 
     // Turn the LED off
-    gpio.write_GPIO(LED_PORT, LED_PIN, LOW);
-    Serial.println("Set PA21 to LOW");
+    gpio.write_GPIO(LED1_PORT, LED1_PIN, LOW);
+    gpio.write_GPIO(LED2_PORT, LED2_PIN, LOW);
+    gpio.write_GPIO(LED3_PORT, LED3_PIN, LOW);
+    Serial.println("All 3 LED to LOW");
 
-    // Wait for 500ms
-    timer.wait(500);
+    timer.wait(1000);
+    
 }
